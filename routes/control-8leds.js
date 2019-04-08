a0403a3e9172084027dbdc206c803d9d3f052205
hmx4 <hungmh1@gmail.com>
2019-04-08 22:34:50 +0800

    update control-8leds.js

--- routes/control-8leds.js
+++ routes/control-8leds.js
@@ -20,93 +20,101 @@ var leds = [led1, led2, led3, led4, led5, led6, led7, led8];
 // set values of the GPIO pins
 function SetLEDsValues(value1, value2, value3, value4, value5, value6, value7, value8)
 {
-	led1.writeSync(value1);
-	led2.writeSync(value2);
-	led3.writeSync(value3);
-	led4.writeSync(value4);
-	led5.writeSync(value5);
-	led6.writeSync(value6);
-	led7.writeSync(value7);
-	led8.writeSync(value8);
+		led1.writeSync(value1);
+		led2.writeSync(value2);
+		led3.writeSync(value3);
+		led4.writeSync(value4);
+		led5.writeSync(value5);
+		led6.writeSync(value6);
+		led7.writeSync(value7);
+		led8.writeSync(value8);
 }
 
 // Unexport the the GPIO pins
 function UnexportLEDs()
 {
-	led1.unexport();
-	led2.unexport();
-	led3.unexport();
-	led4.unexport();
-	led5.unexport();
-	led6.unexport();
-	led7.unexport();
-	led8.unexport();
+		led1.unexport();
+		led2.unexport();
+		led3.unexport();
+		led4.unexport();
+		led5.unexport();
+		led6.unexport();
+		led7.unexport();
+		led8.unexport();
 }
 
 // blink the odd-number LEDS for 6 times
-function Blink_Odd_LEDs()
-{
-	var i;
-    for (i = 0; i < 6; i++) { 
-		setTimeout(function() {
-			// turn the odd leds on after 0.2 second
-			SetLEDsValues(1, 0, 1, 0, 1, 0, 1, 0);
-		}, 200);
+function Blink_Odd_LEDs() {
+		//run the blinkOddLEDs function every 200ms
+		var blinkOddLEDsInterval = setInterval(blinkOddLEDs, 200); 
+		//stop blinking after 1.2 seconds
+		setTimeout(endBlinkLEDs(blinkOddLEDsInterval), 1200); 
+}
 
-		setTimeout(function() {
-			// turn all leds off after 0.2 second
-			SetLEDsValues(0, 0, 0, 0, 0, 0, 0, 0);
-		}, 200);
-	}
+//function to start blinking odd LEDs
+function blinkOddLEDs(){ 
+		if (led1.readSync() === 0) { //check the led1's state, if the state is 0
+				SetLEDsValues(1, 0, 1, 0, 1, 0, 1, 0); // turn the odd LEDs on
+		} else {
+				SetLEDsValues(0, 0, 0, 0, 0, 0, 0, 0); // turn all of the LEDs off
+		}
 }
 
 // blink the even-number LEDS for 6 times
-function Blink_Even_LEDs()
-{
-	var i;
-    for (i = 0; i < 6; i++) { 
-		setTimeout(function() {
-			// turn the even leds on after 0.2 second
-			SetLEDsValues(0, 1, 0, 1, 0, 1, 0, 1);
-		}, 200);
+function Blink_Even_LEDs() {
+	//run the blinkEvenLEDs function every 200ms
+	var blinkEvenLEDsInterval = setInterval(blinkOddLEDs, 200); 
+	//stop blinking after 1.2 seconds
+	setTimeout(endBlinkLEDs(blinkEvenLEDsInterval), 1200); 
+}
 
-		setTimeout(function() {
-			// turn all leds off after 0.2 second
-			SetLEDsValues(0, 0, 0, 0, 0, 0, 0, 0);
-		}, 200);
+//function to start blinking even LEDs
+function blinkEvenLEDs(){ 
+	if (led1.readSync() === 0) { //check the led1's state, if the state is 0
+			SetLEDsValues(0, 1, 0, 1, 0, 1, 0, 1); // turn the even LEDs on
+	} else {
+			SetLEDsValues(0, 0, 0, 0, 0, 0, 0, 0); // turn all of the LEDs off
 	}
 }
 
 // Run the LEDS for 3 times
 function Run_LEDs()
 {
-	var indexCount = 0;   //a counter
-	var dir = "left";     //variable for running direction
-	var i;
-	for (i = 0; i < 3; i++) 
-	{ 
-		setTimeout(function() {
-			leds.forEach(function(currentLED) { //for each item in array
-				currentLED.writeSync(0);        //turn off LED
-			});
-			if (indexCount == 0) dir = "left"; //set running direction to "left" if the count reaches zero
-			if (indexCount >= leds.length) dir = "right"; //set running direction to "right" if the count reaches 7
-			if (dir == "right") indexCount--; //count downwards if direction is right
-			leds[indexCount].writeSync(1); //turn on LED that where array index matches count
-			if (dir == "left") indexCount++ //count upwards if direction is left
-		}, 50);
-	}
+		var indexCount = 0;   //a counter
+		var dir = "left";     //variable for running direction
+		//run the blinkEvenLEDs function every 200ms
+		var runLEDsInterval = setInterval(runLEDS, 200); 
+		//stop blinking after 1.2 seconds
+		setTimeout(endBlinkLEDs(runLEDsInterval), 1200); 
 }
 
+function runLEDs() {
+		leds.forEach(function(currentLED) { //for each item in array
+			currentLED.writeSync(0);        //turn off LED
+		});
+		if (indexCount == 0) dir = "left"; //set running direction to "left" if the count reaches zero
+		if (indexCount >= leds.length) dir = "right"; //set running direction to "right" if the count reaches 7
+		if (dir == "right") indexCount--; //count downwards if direction is right
+		leds[indexCount].writeSync(1); //turn on LED that where array index matches count
+		if (dir == "left") indexCount++ //count upwards if direction is left
+}
+
+// function to stop a blink interval and unexport the GPIOs of all LEDs
+function endBlinkLEDs(intervalID) 
+{ 
+		clearInterval(intervalID); // Stop blink intervals
+		SetLEDsValues(0, 0, 0, 0, 0, 0, 0, 0); // turn all of the LEDs off
+		UnexportLEDs(); // Unexport GPIO to free resources
+}
 // Listen to the event triggered on CTRL+C
-process.on('SIGINT', function () { 
-	// Cleanly close the GPIO pins before exiting
-	leds.forEach(function(currentLED) {   //for each LED
-		currentLED.writeSync(0);          //turn off LED
-		currentLED.unexport();            //unexport GPIO
+process.on('SIGINT', function () 
+{ 
+		// Cleanly close the GPIO pins before exiting
+		leds.forEach(function(currentLED) {   //for each LED
+				currentLED.writeSync(0);          //turn off LED
+				currentLED.unexport();            //unexport GPIO
 	  });
-  	console.log('Bye, bye!');
-  	process.exit();
+  	
 });
 
 // 控制8顆LED燈之函數
